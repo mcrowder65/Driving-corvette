4a09851d710054cdf1f3c6f7f65f883ed53181c0
Matt Crowder <mcrowder65@gmail.com>
2016-10-13 15:55:18 -0600

    got rid of reference object

--- Car.h
+++ Car.h
@@ -20,23 +20,21 @@ private:
     Object car;
     Object reference;
 public:
-    Car(FrontTire frontLeftTire, FrontTire frontRightTire, Object backLeftTire, Object backRightTire, Object baymax, Object car, Object reference) {
+    Car(FrontTire frontLeftTire, FrontTire frontRightTire, Object backLeftTire, Object backRightTire, Object baymax, Object car) {
         this->frontLeftTire = frontLeftTire;
         this->frontRightTire = frontRightTire;
         this->backLeftTire = backLeftTire;
         this->backRightTire = backRightTire;
         this->baymax = baymax;
         this->car = car;
-        this->reference = reference;
     }
-    void draw(Params fLParams, Params fRParams, Params bLParams, Params bRParams, Params baymaxParams, Params carParams, Params referenceParams) {
+    void draw(Params fLParams, Params fRParams, Params bLParams, Params bRParams, Params baymaxParams, Params carParams) {
         frontLeftTire.draw(fLParams);
         frontRightTire.draw(fRParams);
         backLeftTire.draw(bLParams);
         backRightTire.draw(bRParams);
         baymax.draw(baymaxParams);
         car.draw(carParams);
-        reference.draw(referenceParams);
     }
 };
 #endif /* Car_h */
